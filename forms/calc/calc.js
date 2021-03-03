b6b99d32ca83713744e800c47cf59af9f6424bb6
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-03 16:11:11 -0600

    Create calc.js

---
+++ forms/calc/calc.js
@@ -0,0 +1,12 @@
+function Calculate (num1, num2){
+   let total = num1 + num2
+   return total;
+   }
+   
+let n1 = parseInt(prompt("Please enter an integer "))
+let n2 = parseInt(prompt("Please enter an integer "))
+
+
+let summedNumbers = Calculate(n1,n2)
+
+console.log(`The sum of ${n1} and ${n2} is ${summedNumbers}`)
