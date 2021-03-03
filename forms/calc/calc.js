0e8c01c5b378615dab7a57ce2e78ad8e1a419d4a
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-03 12:58:23 -0600

    Create calc.js

---
+++ forms/calc/calc.js
@@ -0,0 +1,12 @@
+function Calculate (num1, num2){
+   let total = num1 + num2
+   return total;
+   }
+   
+let n1 = prompt("Please enter an integer ")
+let n2 = prompt("Please enter an integer ")
+
+
+let summedNumbers = Calculate(n1,n2)
+
+console.log(summedNumbers)
