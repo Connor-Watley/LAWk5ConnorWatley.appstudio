57b2895c2449e99b7b1de5b141be2ad3bfca0a71
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-03 13:00:01 -0600

    Update calc.js

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -3,8 +3,8 @@ function Calculate (num1, num2){
    return total;
    }
    
-let n1 = prompt("Please enter an integer ")
-let n2 = prompt("Please enter an integer ")
+let n1 = parseInt(prompt("Please enter an integer "))
+let n2 = parseInt(prompt("Please enter an integer "))
 
 
 let summedNumbers = Calculate(n1,n2)
